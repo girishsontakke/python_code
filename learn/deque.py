2754f153aa790df94d2c20e6aa5835ff7de4ab91
girishsontakke <61848210+girishsontakke@users.noreply.github.com>
2020-05-03 17:18:08 +0530

    update deque

--- learn/deque.py
+++ learn/deque.py
@@ -24,3 +24,33 @@ print(de2.count(2))
 print("de2 after removing 4 from first occurance")
 de2.remove(4)
 print(de2)
+# using extend() to add numbers to right end  
+# adds 4,5,6 to right end 
+de.extend([4,5,6]) 
+  
+# printing modified deque 
+print ("The deque after extending deque at end is : ") 
+print (de) 
+  
+# using extendleft() to add numbers to left end  
+# adds 7,8,9 to right end 
+de.extendleft([7,8,9]) 
+  
+# printing modified deque 
+print ("The deque after extending deque at beginning is : ") 
+print (de) 
+  
+# using rotate() to rotate the deque 
+# rotates by 3 to left 
+de.rotate(-3) 
+  
+# printing modified deque 
+print ("The deque after rotating deque is : ") 
+print (de) 
+  
+# using reverse() to reverse the deque 
+de.reverse() 
+  
+# printing modified deque 
+print ("The deque after reversing deque is : ") 
+print (de) 
