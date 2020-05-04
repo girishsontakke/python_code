42c1b3b4523d83275bf5885b7e1e3c8977c91f39
girishsontakke <61848210+girishsontakke@users.noreply.github.com>
2020-05-04 19:17:16 +0530

    delete temcoderunner file

--- learn/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-  # for k in range(len(temp)):
-    #     if k % 2 == 0:
-    #         print(temp[k], end=" ")
-    # for k in range(len(temp)):
-    #     if k % 2 != 0:
-    #         print(temp[k], end="")
-    # print("\n")
