d49b43851c9d7928331b3e32a51815cc939fed00
girishsontakke <61848210+girishsontakke@users.noreply.github.com>
2020-05-04 19:30:08 +0530

    upload iterable file

---
+++ learn/iterable.py
@@ -0,0 +1,6 @@
+lst = [1, 3, 5, 4, 7]
+obj = iter(lst)#generates obj as iterator from lst(iterable)
+print(next(obj))#generate new iterator from previosly generated iterable
+print(next(obj))
+print(next(obj))
+print(next(obj))
