b48f394c87960854ddfc26d168d8c1074b1b68a2
girishsontakke <61848210+girishsontakke@users.noreply.github.com>
2020-05-05 10:12:19 +0530

    update

--- Telusko/search.py
+++ Telusko/search.py
@@ -1,13 +1,12 @@
-list = [1,2,3,4,5,6,7]
-n =int(input("Enter a number to be searched in list: "))
+lst = [1, 2, 3, 4, 5, 6, 7]
+n = int(input("Enter a number to be searched in list: "))
 count = 0
 
-for i in range(len(list)):
-    if list[i] == n:
+for i in range(len(lst)):
+    if lst[i] == n:
         count += 1
 
-if count != 0 :
+if count != 0:
     print(str(n) + " is in list")
 else:
-     print(str(n) + " is not in list")
-    
+    print(str(n) + " is not in list")
