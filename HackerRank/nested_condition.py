19419ed1ad986bed3f28e93bdd13de3129550c8e
girishsontakke <61848210+girishsontakke@users.noreply.github.com>
2020-05-12 21:06:19 +0530

    updated solution using datetime module

--- HackerRank/nested_condition.py
+++ HackerRank/nested_condition.py
@@ -1,30 +1,25 @@
 import datetime
-class Library:
-   def __init__(self, return_date, expected_date):
-      self.return_date = return_date
-      self.expected_date = expected_date
-   
-   def calculate_fine(self):
-      if self.return_date[2] <= self.expected_date[2]:
-         if self.return_date[2] == self.expected_date[2]:
-            if self.return_date[1] <= self.expected_date[1]:
-               if self.return_date[1] == self.expected_date[1]:
-                  if self.return_date[0] <= self.expected_date[0]:
-                     return 0
-                  else:
-                     return 15 *(self.return_date[0] - self.expected_date[0])
-               else:
-                  return 0
-            else:
-               return 500 *(self.return_date[1] - self.expected_date[1])
-         else:
-            return 0
-      else:
-         return 10000     
-         
 
-rdate = list(map(int, input().strip().split()))
-edate = list(map(int, input().strip().split()))
+
+class Library:
+    def __init__(self, return_date, expected_date):
+        self.return_date = datetime.datetime.strptime(return_date, '%d %m %Y')
+        self.expected_date = datetime.datetime.strptime(expected_date, '%d %m %Y')
+
+    def calculate_fine(self):
+        if self.return_date > self.expected_date:
+            if self.return_date.year == self.expected_date.year:
+                if self.return_date.month == self.expected_date.month:
+                    return 15 * (self.return_date.day - self.expected_date.day)
+                else:
+                    return 500 * (self.return_date.month - self.expected_date.month)
+            else:
+                return 10000
+        else:
+            return 0
+
+
+rdate = input()
+edate = input()
 library = Library(rdate, edate)
 print(library.calculate_fine())
-
