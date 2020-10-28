07870cc16c5f5433af6d4fc92e5622f66dc2c6b5
girishsontakke <girishsontakke7@gmail.com>
2020-10-28 16:51:10 +0530

    optimise solution

--- HackerRank/exception.py
+++ HackerRank/exception.py
@@ -1,25 +1,13 @@
-# Write your code here
-class Calculator:
-    def power(self, n, p):
-        self.n = n
-        self.p = p
-        try:
-            if n >= 0 and p >= 0:
-                a = self.n ** self.p
-                return a
-        except:
-            a = "n and p should be non-negative"
-            return a   
-# class e(Exception):
-#     def __init__(self):
-#         Exception.__init__(self,"n and p should be non-negative")
+class e(Exception):
+    def __init__(self):
+        Exception.__init__(self,"n and p should be non-negative")
 
-# class Calculator:
-#     def power(self,n,p):
-#         if  n < 0 or p < 0:
-#             raise e
-#         else:
-#             return n**p
+class Calculator:
+    def power(self,n,p):
+        if  n < 0 or p < 0:
+            raise e
+        else:
+            return n**p
 
 myCalculator = Calculator()
 T = int(input())
