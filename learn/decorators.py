5ab8f1ab9e7cf2bbfa616bb16ed88f953d3585ef
girishsontakke <girishsontakke7@gmail.com>
2021-03-29 22:32:01 +0530

    implementation of wraps and update_wrapper from functools

--- learn/decorators.py
+++ learn/decorators.py
@@ -1,6 +1,9 @@
+from functools import wraps, update_wrapper
 # Decorators
 
+
 def decorator_function(original_function):
+    @wraps(original_function)
     def wrapper_function(*args, **kwargs):
         print(f"wrapper executed this before {original_function.__name__}")
         return original_function(*args, **kwargs)
@@ -8,8 +11,11 @@ def decorator_function(original_function):
 
 
 class decorator_class(object):
+    ''' documentation for wrapper class '''
+
     def __init__(self, original_function):
         self.original_function = original_function
+        update_wrapper(self, original_function)
 
     def __call__(self, *args, **kwargs):
         print(
@@ -30,9 +36,11 @@ def display():
 
 @decorator_class
 def display_info(name, age):
+    ''' return display info '''
     print(f'display info {name} {age}')
 
 
 print("\nby decorators")
 display()
 display_info("girish", 20)
+print(display_info.__doc__)
